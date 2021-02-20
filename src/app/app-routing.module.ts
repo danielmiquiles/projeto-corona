fdeaa9c4d880ee31d4e4b983298054cce5b7e162
YasmimVieira <vieiray937@gmail.com>
2021-02-20 11:10:08 -0300

    chore: add region component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RegionComponent } from './pages/region/region.component';
+
+const routes: Routes = [
+  { path: 'region', component: RegionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
